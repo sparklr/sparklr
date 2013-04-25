ca740cdde24a87969cf3e9c2d6ed45ed1d52d71c
Jonathan <jaxbot@societyofcode.com>
2013-04-24 20:41:21 -0400

    snapshot

--- frontend.js
+++ frontend.js
@@ -3,55 +3,81 @@ var templates = require("./templates");
 var database = require("./database");
 var Post = require("./post");
 var User = require("./user");
+var async = require("async");
+
+var frontendTemplate = "";
+
+fs.readFile("./frontend.html", function(err,data) {
+var s = data.toString().split("<require ");
+	s = s[1].split(">");
+	s = s[0].split(",");
+	var scripts = "";
+	for (var i = 0; i < s.length; i++)
+		scripts += "<script src='"+global.commonHost+s[i]+"'></script>";
+	
+	data = data.toString().replace("</head>", scripts + "</head>");
+
+	frontendTemplate = data.toString();
+});
 
 exports.run = function(user, request, response, sessionid) {
 	response.writeHead(200, { "Content-type": "text/html" });
 	var sessiondata = sessionid.split(",");
 	var authkey = sessiondata[1];
 
-	fs.readFile("./frontend.html", function(err,data) {
-		data = data.toString();
-		var s = data.split("<require ");
-		s = s[1].split(">");
-		s = s[0].split(",");
-		var scripts = "";
-		for (var i = 0; i < s.length; i++)
-			scripts += "<script src='"+global.commonHost+s[i]+"'></script>";
-
+	var data = frontendTemplate;
 		eval(templates.parse(data.toString()));
-		html = html.replace("</head>", scripts + "</head>");
-		html = "<!-- Instancy-Luna Prototype -->" + html;
-	
-	
-		user.following = user.following.split(",").filter(function(e) { return e; });
-		user.followers = user.followers.split(",").filter(function(e) { return e; });
+	html = "<!-- Instancy-Luna Prototype -->" + html;
 
-		var from = user.following;
-		from.push(user.id);
 
-		database.getStream("timeline", { from: from }, function(err,stream) {
-			var timelineStream;
-				timelineStream = JSON.stringify(stream);
-			var commentCounts;
-			Post.getCommentCounts(stream, function(err,rows) {
-				commentCounts = rows;
-			});
+	var friends = [];
+	for (i in user.following) {
+		if (user.followers.indexOf(user.following[i]) !== -1)
+			friends.push(user.following[i]);
+	}
 
-			User.getMassUserDisplayName(from, function (err, names) {
-				var displayNames = {};
-				var userHandles = {};
+	user.following = user.following.split(",").filter(function(e) { return e; });
+	user.followers = user.followers.split(",").filter(function(e) { return e; });
 
-				for (name in names) {
-					displayNames[names[name].id] = names[name].displayname;
-					userHandles[names[name].id] = names[name].username;
-				}
+	var from = user.following;
+	from.push(user.id);
 
-				var friends = [];
-				for (i in user.following) {
-					if (user.followers.indexOf(user.following[i]) !== -1)
-						friends.push(user.following[i]);
-				}
-				
+	async.parallel([			function (callback) { 
+				database.getStream("timeline", { from: from }, function(err,stream) {
+					var timelineStream;
+					timelineStream = JSON.stringify(stream);
+
+					html = html.replace("<! timelineStream ->", timelineStream);
+					Post.getCommentCounts(stream, function(err,rows) {
+
+						var commentCounts = rows;
+
+						html = html.replace("<! commentCounts ->", JSON.stringify(commentCounts));
+
+						callback();
+					});
+
+				});
+
+			},
+			function (callback) { 
+				User.getMassUserDisplayName(from, function (err, names) {
+					var displayNames = {};
+					var userHandles = {};
+
+					for (name in names) {
+						displayNames[names[name].id] = names[name].displayname;
+						userHandles[names[name].id] = names[name].username;
+					}
+
+					html = html.replace("<! displayNames ->", JSON.stringify(displayNames));
+					html = html.replace("<! userHandles ->", JSON.stringify(userHandles));
+
+					callback();
+				});
+
+			},
+			function (callback) { 	
 				User.getOnlineFriends(friends, function(err,onlinefriends) {
 					var friendsObj = {};
 					for (i in friends) {
@@ -61,21 +87,14 @@ exports.run = function(user, request, response, sessionid) {
 						friendsObj[onlinefriends[i].id] = true;
 					}
 
-					html = html.replace("<! timelineStream ->", timelineStream);
-					
-					html = html.replace("<! commentCounts ->", JSON.stringify(commentCounts));
-					html = html.replace("<! displayNames ->", JSON.stringify(displayNames));
-					html = html.replace("<! userHandles ->", JSON.stringify(userHandles));
+
+
 					html = html.replace("<! friends ->", JSON.stringify(friendsObj));
 
-					response.write(html);
-					response.end();
-
+					callback();
 				});
+			}], function () {
+				response.write(html);
+				response.end();
 			});
-			
-		});
-
-	
-	});
 }
