c7fda9d15685102147750ca527ffe02777eb2920
Jonathan <jaxbot@gmail.com>
2013-06-25 08:32:58 -0400

    Change limit from 30 to 20

--- backend/database.js
+++ backend/database.js
@@ -98,7 +98,7 @@ exports.getStream = function(table, args, callback) {
 		query += ") AND (`time` < "+parseInt(args.starttime);
 	}
 	
-	query += ") ORDER BY `time` DESC LIMIT 30";
+	query += ") ORDER BY `time` DESC LIMIT 20";
 	exports.query(query, callback);
 //	console.log(query);
 }
