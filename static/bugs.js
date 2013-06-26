f1cda24de1d29ef59980d00804d8ac66537f8e1b
Jonathan <jaxbot@societyofcode.com>
2013-06-26 10:10:44 -0400

    Feedback use popup

--- static/bugs.js
+++ static/bugs.js
@@ -1,9 +1,9 @@
 function sendFeedback(msg) {
+	hideAllPopups();
 	ajaxGet("work/feedback", { msg: msg }, function() {
-		_g("feedback").style.display = "none";
 		showBanner("Your feedback has been sent successfully.", "feedbackbanner", 5000);
 	});
 }
 function showFeedbackForm() {
-	_g("feedback").style.display = "block";
+	showPopup(_g("feedback").innerHTML);
 }
