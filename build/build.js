b3522a37311b28549888704e57e27529553fcbc9
Jonathan <jaxbot@societyofcode.com>
2013-06-27 14:03:18 -0400

    Add CSS Sprites

--- build/build.js
+++ build/build.js
@@ -1,26 +1,8 @@
-var fs = require("fs");
+// Main entry for p18 build script
 
-// Compile the templates
+var cssBuilder = require("./css");
+var templatesBuilder = require("./templates");
 
-var templateData = "";
-var templateFormatter = require("../static/templates.js");
-
-fs.readdir("../static/templates", function(err, files) {
-	for (var i = 0; i < files.length; i++) {
-		console.log("Compiling " + files[i]);
-		var data = fs.readFileSync("../static/templates/" + files[i]);
-		var id = files[i].split(".");
-			id = id[0];
-
-		data = templateFormatter.t(data.toString());
-		// escape 
-		data = data.replace(/\\/g, "\\\\");
-		data = data.replace(/\"/g, "\\\"");
-
-		templateData += "TEMPLATES['"+id+"'] = \"" + data + "\";\n";
-	}
-
-	console.log("Writing ../static/out/templates.js");
-	fs.writeFile("../static/out/templates.js", templateData);
-});
+templatesBuilder.build();
+cssBuilder.build();
 
