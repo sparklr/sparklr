afa006cfd6c8aa518f0193e00eefe52c10231bf4
Jonathan <jaxbot@gmail.com>
2013-06-26 21:38:45 -0400

    Check if feedback msg is empty

--- static/bugs.js
+++ static/bugs.js
@@ -1,4 +1,5 @@
 function sendFeedback(msg) {
+	if (msg == "") return;
 	hideAllPopups();
 	ajaxGet("work/feedback", { msg: msg }, function() {
 		showBanner("Your feedback has been sent successfully.", "feedbackbanner", 5000);
