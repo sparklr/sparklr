f77a8510d8166e662befce96fcfabcb023bb67d3
Jonathan <jaxbot@societyofcode.com>
2013-07-05 16:40:52 -0400

    Support using signals to reload app

---
+++ backend/backend.js
@@ -0,0 +1,56 @@
+var cluster = require("cluster");
+
+var url = require("url");
+var http = require("http");
+var Cookies = require("cookies");
+
+var frontend = require("./frontend");
+var user = require("./user");
+var work = require("./work");
+var database = require("./database");
+
+var memwatch = require('memwatch');
+var hd = new memwatch.HeapDiff();
+memwatch.on("leak", function(info) {
+	var diff = hd.end();
+	console.log(diff);
+});
+
+require("./config");
+
+database.init(global.database);
+global.broker = [];
+global.broker_set = function(key) {
+	process.send({ key: key, value: global.broker[key] });
+}
+var server = http.createServer(function(request,response) {
+	var requesturi = url.parse(request.url, true);
+	var cookies = new Cookies(request,response);
+	var sessionid = cookies.get("D");
+
+	if (requesturi.pathname.indexOf("reload") !== -1) process.send("reload");
+
+	if (requesturi.pathname.indexOf("/work") !== -1 || requesturi.pathname.indexOf("/beacon") !== -1) {
+		work.run(request,response,requesturi,sessionid);
+	} else {
+		if (sessionid != null && sessionid != "") {
+			var s = sessionid.split(",");
+
+			user.verifyAuth(s[0],s[1], function(success,userobj) {
+				if (success)
+					frontend.run(userobj,request,response,sessionid);
+				else
+					frontend.showExternalPage(request,response);
+			});
+		} else {
+			frontend.showExternalPage(request,response);
+		}
+	}
+});
+server.listen(8080);
+process.on('uncaughtException', function(err) {
+	console.log(err);
+	console.log(err.stack);
+	process.exit(1);
+});
+
