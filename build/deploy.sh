e5b20ce42033463aeb9937e3b676918856447cf5
Jonathan <jaxbot@societyofcode.com>
2013-07-20 20:22:26 -0400

    Add build hook to repo

---
+++ build/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Git Deployment Script
+echo "P18 Deployment Script Initiated"
+
+cd /var/www/p18 || exit
+
+unset GIT_DIR
+git fetch --all
+git reset --hard origin/master
+
+cp /var/www/p18_config.js /var/www/p18/backend/config.js
+
+cd build
+node build.js frontend
+
+MESSAGE=`git log -1 HEAD --pretty=format:%s)`
+MESSAGE=${MESSAGE//\"/\\\"}
+MESSAGE=${MESSAGE//'/\\'}
+
+echo "use p18; INSERT INTO \`timeline\` (\`from\`, \`time\`, \`message\`) VALUES (12, `date +%s`, \"$MESSAGE\" )" | mysql -u root -pCe3265Bb
+
+cd /var/www/p18mobile || exit
+unset GIT_DIR
+git fetch --all
+git reset --hard origin/mobile
+
+cd build
+node build.js mobile
+
+# reboot server
+kill -USR2 `cat /var/www/luna.pid`
