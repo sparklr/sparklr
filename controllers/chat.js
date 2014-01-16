cab955b426bcace742de315c9fcfa366f84a7c2f
Jonathan <jaxbot@gmail.com>
2014-01-16 12:36:21 -0500

    Change up the controllers, fix chat

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,24 +1,23 @@
-function before(data, fragments, scope) {
-	scope.user = fragments[2] || fragments[1];
-	scope.convoid = user + "," + CURUSER;
+function before(scope) {
+	scope.user = scope.fragments[1] || scope.fragments[0];
+	scope.convoid = scope.user + "," + CURUSER;
 }
 
-function after(data, fragments, scope) {
+function after(scope) {
 	chatTimes[scope.convoid] = [];
 
-	for (var i = data.length - 1; i >= 0; i--) {
-		addChatMessage(data[i].from, data[i].to, data[i].message, data[i].time, false);
+	for (var i = scope.data.length - 1; i >= 0; i--) {
+		addChatMessage(scope.data[i].from, scope.data[i].to, scope.data[i].message, scope.data[i].time, false);
 	}
 
-	var e = _g('scrollUpContent_'+convoid);
+	var e = _g('scrollUpContent_'+scope.convoid);
 	e.addEventListener("DOMMouseScroll", scrollUpHandler);
 	e.addEventListener("mousewheel", scrollUpHandler);
-	_g("composer_chat_"+user).focus();
+	_g("composer_chat_"+scope.user).focus();
 	window.onload = function() {
-		_g("scrollUpContent_"+convoid).scrollTop = 0xFFFFFF; 
+		_g("scrollUpContent_"+scope.convoid).scrollTop = 0xFFFFFF; 
 	};
 
-	setWindowTitle('m'+convoid, getDisplayName(user));
+	setWindowTitle('m'+scope.convoid, getDisplayName(scope.user));
 }
 
-//TODO: USE EXPORTS
