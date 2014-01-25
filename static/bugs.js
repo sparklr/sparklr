3b8d3d4cc9ff798ccf449cb040a69758003b4277
Jonathan <jaxbot@societyofcode.com>
2014-01-25 18:22:46 -0500

    Fix bugs reporting, make bugs include user agent

--- static/bugs.js
+++ static/bugs.js
@@ -5,6 +5,9 @@
 function sendFeedback(msg) {
 	if (msg == "") return;
 	hideAllPopups();
+
+	msg += "\n" + navigator.userAgent;
+
 	ajax("feedback", { msg: msg }, function() {
 		showBanner("Your feedback has been sent successfully.", "feedbackbanner", 5000);
 	});
