c0dae1f76e8830973cac6b2b66cdf930c06fbb30
ivey <iveysaurrr@gmail.com>
2014-01-28 21:54:11 -0500

    Remove main branch files

--- controllers/settings.js
+++
@@ -1,8 +0,0 @@
-function after(scope) {
-	USERHANDLES[CURUSER] = scope.data.username;
-	var blacklist = (scope.data.blacklist || "").split(",");
-	for (i in blacklist) {
-		if (!blacklist[i]) continue;
-		addUserToList(0, blacklist[i]);
-	}
-}
