2f987b72ac3b5dfb36e1bd5de486c76e466dc670
ivey <iveysaurrr@gmail.com>
2014-01-28 21:54:11 -0500

    Remove main branch files

--- controllers/notifications.js
+++
@@ -1,6 +0,0 @@
-function after() {
-	for(var i in currentNotifications){
-		if(!currentNotifications[i]) continue;
-		addNotificationToPage(currentNotifications[i]);
-	}
-}
--- controllers/search.js
+++
@@ -1,7 +0,0 @@
-function after(scope) {
-	if (scope.data.posts) {
-	for (var i = scope.data.posts.length-1; i >=0; i--) {
-			addTimelineEvent(scope.data.posts[i]);
-		}
-	}
-}
