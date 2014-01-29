048dde539c213c1a97c00489167ba7a44a0e878a
Jonathan <jaxbot@gmail.com>
2014-01-28 21:22:30 -0500

    Update SQL file
    Cleaned up unused rows and reset default accounts to default
    information, password = test

--- p18.sql
+++ p18.sql
@@ -88,7 +88,6 @@ CREATE TABLE `timeline` (
   `network` varchar(20) NOT NULL DEFAULT '0',
   `type` int(11) NOT NULL DEFAULT '0',
   `meta` tinytext,
-  `tags` text,
   `time` int(11) NOT NULL,
   `public` int(11) DEFAULT NULL,
   `message` text NOT NULL,
@@ -96,7 +95,6 @@ CREATE TABLE `timeline` (
   `via` int(11) DEFAULT NULL,
   `commentcount` int(11) DEFAULT NULL,
   `modified` int(11) DEFAULT NULL,
-  `blockcount` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `modified` (`modified`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -112,23 +110,18 @@ CREATE TABLE `users` (
   `bio` text,
   `avatarid` int(11) DEFAULT NULL,
   `lastseen` int(11) DEFAULT NULL,
-  `followers` text,
   `following` text,
   `blacklist` text,
-  `whitelist` text,
-  `private` tinyint(4) DEFAULT NULL,
-  `emailverified` tinyint(4) DEFAULT NULL,
   `rank` tinyint(4) DEFAULT NULL,
   `networks` tinytext,
-  `background` int(11) DEFAULT NULL,
   `authkey` tinytext,
+  `emailverified` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`(30))
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `displayname`, `bio`, `avatarid`, `lastseen`, `followers`, `following`, `blacklist`, `whitelist`, `private`, `emailverified`, `rank`, `networks`, `background`, `authkey`) VALUES
-(4,	'jaxbot',	'jaxbot@societyofcode.com',	'$2a$11$PlUACuZyK7Oi/QNgT6wSPOC3B4Hv2Vwc29hnlyW5DM.h/xzexI6k.',	'Jonathan warner 2âœ»',	'hi im jonathan hehe defsfsd fdsfdfdssdf',	1380993255,	1378173008,	'8,5,1,1,40,41,42,43,44,45,46,48,49,51,52,6',	'1,5,43,4,44,45,46,48,51,52,6,55',	'',	NULL,	0,	0,	50,	'0',	1380993260,	'4dab1a8afdfca30ed4492016f9db397df38353652'),
-(6,	'ivey',	'iveysaurrr@gmail.com',	'$2a$11$PlUACuZyK7Oi/QNgT6wSPOC3B4Hv2Vwc29hnlyW5DM.h/xzexI6k.',	'ivey',	'I love you~ what~~~',	1381233913,	1377919665,	'1,48,4',	'',	'',	NULL,	0,	0,	0,	'0',	1378927645,	'$2a$11$eetQaY5qp3Z/9bW8aGofWuXMjHe4pgmKUJkgIPHzapBUNTwDfnVn2'),
-(55,	'ivan',	'jaxbot@gmail.com22',	'$2a$11$8VfrP0JhgZdK1.9NAhAvQ.JrfM6LkYo9l/9dpsURfg7.W5hDdBgd2',	'Ivan Colburnâœ»',	' I\'m a Sparklr Test account, 18, big music fan. Krew for lyfe lol!',	1381000465,	1378334947,	'54',	'54,4,6',	'',	NULL,	0,	NULL,	50,	'0,odd',	1381723244,	'test12b5c4139841d4671c291ddd98162f8736a59577a8');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `displayname`, `bio`, `avatarid`, `lastseen`, `following`, `blacklist`, `rank`, `networks`, `authkey`, `emailverified`) VALUES
+(4,	'jaxbot',	'jaxbot@gmail.com',	'$2a$11$PlUACuZyK7Oi/QNgT6wSPOC3B4Hv2Vwc29hnlyW5DM.h/xzexI6k.',	'Jonathan',	'Jonathan test account',	1390708388,	1378173008,	'55,57',	'',	50,	'0',	'4dab1a8afdfca30ed4492016f9db397df38353652',	NULL),
+(6,	'ivey',	'iveysaurrr@gmail.com',	'$2a$11$PlUACuZyK7Oi/QNgT6wSPOC3B4Hv2Vwc29hnlyW5DM.h/xzexI6k.',	'ivey',	'Ivey test account',	1389580622,	1377919665,	'4',	'',	0,	'0',	'$2a$11$eetQaY5qp3Z/9bW8aGofWuXMjHe4pgmKUJkgIPHzapBUNTwDfnVn2',	NULL);
 
--- 2013-10-24 12:16:16
+-- 2014-01-28 21:20:46
