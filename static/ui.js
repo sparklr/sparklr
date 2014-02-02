871e0a073fb4208a250c5eedaf46d193132109b7
Jonathan Warner <jaxbot@gmail.com>
2014-02-02 10:18:44 -0500

    Switch to using an improved regex expr for links

--- static/ui.js
+++ static/ui.js
@@ -122,9 +122,44 @@ function processPost(post) {
 
 // Parse links, hashtags, @, imgs, etc
 function processMedia(text,noImages) {
-	//TODO: matches too many URLs
 	var linkcount = 0;
-	var urlexp = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:([^\s()<>.]+[.]?)+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'\".,<>?«»“”‘’]))/gi; 
+	// regex via: https://gist.github.com/dperini/729294
+	var urlexp = new RegExp(
+		"^" +
+		// protocol identifier
+		"(?:(?:https?|ftp)://)" +
+		// user:pass authentication
+		"(?:\\S+(?::\\S*)?@)?" +
+		"(?:" +
+		// IP address exclusion
+		// private & local networks
+		"(?!10(?:\\.\\d{1,3}){3})" +
+		"(?!127(?:\\.\\d{1,3}){3})" +
+		"(?!169\\.254(?:\\.\\d{1,3}){2})" +
+		"(?!192\\.168(?:\\.\\d{1,3}){2})" +
+		"(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+		// IP address dotted notation octets
+		// excludes loopback network 0.0.0.0
+		// excludes reserved space >= 224.0.0.0
+		// excludes network & broacast addresses
+		// (first & last IP address of each class)
+		"(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+		"(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+		"(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+		"|" +
+		// host name
+		"(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)" +
+		// domain name
+		"(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*" +
+		// TLD identifier
+		"(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+		")" +
+		// port number
+		"(?::\\d{2,5})?" +
+		// resource path
+		"(?:/[^\\s]*)?" +
+		"$", "i"
+	);
 	text = text.replace(urlexp, function (match) {
 		if (linkcount > 5) return;
 
