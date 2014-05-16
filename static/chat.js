429ae3151c735646181b51185b4d10013d7423d1
Jonathan Warner <jaxbot@gmail.com>
2014-05-16 18:48:05 -0400

    Icon fixes, mobile, etc.

--- static/chat.js
+++ static/chat.js
@@ -108,7 +108,7 @@ function sendChatMessage(e) {
 
 function setNewInbox(value) {
 	var e = _g("inbox");
-	e.className = "inbox" + (value ? " jiggle" : "");
+	e.className = "icon-envelope" + (value ? " jiggle" : "");
 	console.trace();
 }
 
