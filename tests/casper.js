ae421146ef6512fccd305a7c40d0d846bb58a5b0
Jonathan Warner <jaxbot@gmail.com>
2014-08-25 16:22:27 -0400

    Generate username/password
    Update #14 Need to write automated tests

--- tests/casper.js
+++ tests/casper.js
@@ -1,4 +1,4 @@
-casper.test.begin('Sparklr.dev loads a new user', 2, function suite(test) {
+casper.test.begin('Sparklr.dev loads a new user', 5, function suite(test) {
         casper.start("http://sparklr.dev:8080/", function() {
                 test.assertTitle("Sparklr", "Page has correct title");
                 this.click("#welcomebar a");
@@ -10,6 +10,27 @@ casper.test.begin('Sparklr.dev loads a new user', 2, function suite(test) {
                 this.sendKeys('input[name="displayname"]',"Jonathan");
                 this.sendKeys('input[name="email"]',"jaxbot@gmail.com");
                 this.click('#savesettings');
+        });
+        casper.then(function() {
+                this.waitForText("Saved", function() {
+                        test.assertFieldCSS("#savesettings", "Saved", "Settings button changes to saved");
+                });
+        });
+        casper.then(function() {
+                this.click('a[href="#/settings/password"]');
+        });
+        casper.then(function() {
+                test.assertUrlMatch(/password/, 'Password page has been navigated to');
+        });
+        casper.then(function() {
+                this.sendKeys('input[name="password1"]',"test");
+                this.sendKeys('input[name="password2"]',"test");
+                this.click('#savesettings');
+        });
+        casper.then(function() {
+                this.waitForText("Saved", function() {
+                        test.assertFieldCSS("#savesettings", "Saved", "Settings button changes to saved (Passwords)");
+                });
         });
         casper.run(function() {
                 test.done();
