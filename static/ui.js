e3203cc5ec0f04ee9f06e49c25740fe629b04c58
Jonathan Warner <jaxbot@gmail.com>
2014-09-29 14:19:29 -0400

    Fix issue with URL parsing being too restrictive

--- static/ui.js
+++ static/ui.js
@@ -9,7 +9,7 @@ var newPageToFetch = false;
 
 var doctop = 0;
 
-var REGEX_URL = new RegExp('^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$', 'i');
+var REGEX_URL = new RegExp('https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)', 'i');
 
 var REGEX_MENTIONS = /\B@([\w-]+)/gi;
 
